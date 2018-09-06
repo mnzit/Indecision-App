95c21c36176c5245dc099faf22d6da474e379485
mnzit <mnzitshakya@gmail.com>
2018-09-06 09:32:51 +0545

    ES6 classes -2

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -14,10 +14,47 @@ class Person  {
     }
 }
 
-const me = new Person('Manjit Shakya',20);
-console.log(me.getGreeting());
+
+class Student extends Person{
+    constructor(name, age, major){
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor(){
+        return !!this.major;
+    }
+    getDescription(){
+        let description = super.getDescription();
+        if(this.hasMajor()){
+            description += `. Their major is ${this.major}`;
+        }
+        return description;
+    }
+}
+
+const me = new Student('Manjit Shakya',20,'BIT');
 console.log(me.getDescription());
 
-const other = new Person();
-console.log(other.getGreeting());
+const other = new Student();
 console.log(other.getDescription());
+
+class Traveller extends Person{
+    constructor(name, age, homeLocation){
+        super(name,age);
+        this.homeLocation = homeLocation;
+    }
+
+    getGreeting(){
+        let greeting = super.getGreeting();
+        if(this.homeLocation){
+            greeting += `. I'm visiting from ${this.homeLocation}`;
+        }
+        return greeting;
+    }
+}
+
+const travellerNibesh = new Traveller('Nivesh Maharjan', 20, 'Kathmandu');
+console.log(travellerNibesh.getGreeting());
+
+const travellerDikesh = new Traveller();
+console.log(travellerDikesh.getGreeting());
