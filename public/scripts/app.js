d4bbbdff865495b7369c6de6ac9c96a800192d45
mnzit <mnzitshakya@gmail.com>
2018-09-15 13:01:04 +0545

    Indecision State -2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,38 +8,6 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-// const obj = {
-//     name : "Manjit",
-//     getName(){
-//        return this.name;
-//     }
-// };
-// const getName = obj.getName.bind({name: "Kilke"});
-
-// console.log(getName());
-// class MouseTracker extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.handleMouseMove = this.handleMouseMove.bind(this);
-//       this.state = { x: 0, y: 0 };
-//     }
-
-//     handleMouseMove(event) {
-//       this.setState({
-//         x: event.clientX,
-//         y: event.clientY
-//       });
-//     }
-
-//     render() {
-//       return (
-//         <div style={{ height: '100%' }} onMouseMove={this.handleMouseMove}>
-//           <h1>Move the mouse around!</h1>
-//           <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
-//         </div>
-//       );
-//     }
-//   }
 var IndecisionApp = function (_React$Component) {
     _inherits(IndecisionApp, _React$Component);
 
--- src/app.js
+++ src/app.js
@@ -1,35 +1,4 @@
-// const obj = {
-//     name : "Manjit",
-//     getName(){
-//        return this.name;
-//     }
-// };
-// const getName = obj.getName.bind({name: "Kilke"});
 
-// console.log(getName());
-// class MouseTracker extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.handleMouseMove = this.handleMouseMove.bind(this);
-//       this.state = { x: 0, y: 0 };
-//     }
-  
-//     handleMouseMove(event) {
-//       this.setState({
-//         x: event.clientX,
-//         y: event.clientY
-//       });
-//     }
-  
-//     render() {
-//       return (
-//         <div style={{ height: '100%' }} onMouseMove={this.handleMouseMove}>
-//           <h1>Move the mouse around!</h1>
-//           <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
-//         </div>
-//       );
-//     }
-//   }
 class IndecisionApp extends React.Component {
     constructor(props){
         super(props);
