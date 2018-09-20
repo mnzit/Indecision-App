4b7305e234a0c192ccf1081efa5e06bc81f1075d
mnzit <mnzitshakya@gmail.com>
2018-09-20 17:40:17 +0545

    Lifecycle methods

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,6 +27,21 @@ var IndecisionApp = function (_React$Component) {
     }
 
     _createClass(IndecisionApp, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            console.log('Fetching data !');
+        }
+    }, {
+        key: 'componentDidUpdate',
+        value: function componentDidUpdate(prevProps, prevState) {
+            console.log('Saving data !');
+        }
+    }, {
+        key: 'componentWillUnmount',
+        value: function componentWillUnmount() {
+            console.log('Compoenent will unmount !');
+        }
+    }, {
         key: 'handleDeleteOptions',
         value: function handleDeleteOptions() {
             this.setState(function () {
